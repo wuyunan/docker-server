ca541fb4e5924ff741943082ee178278460bf293
wuyunan <wuyunan@qingteng.info>
2019-02-19 12:51:14 +0800

    V1

--- shell/pullcode.sh
+++ shell/pullcode.sh
@@ -1,48 +1,41 @@
 #!/bin/bash
 
-echo "第一个参数$1"
+echo $1
 
-echo "======================== +++++ ========================"
-echo  -e "==========             \033[42;37mpull code\033[0m             =========="
-echo "======================== +++++ ========================"
+echo -e "\033[44;37m ==========         \033[44;37;1mPull Code       \033[0m\033[44;37m========== \033[0m"
 
 gitlabpath="$HOME/Documents/gitlab"
-allow_path_a[0]="wywz"
-allow_path_a[1]="rn"
-allow_path_a[2]="ncwz"
-allow_path_a[3]="qingniu"
-allow_path_a[4]="we"
+
 
 pullPlatform(){
     cd "${gitlabpath}"
-    for project in $(ls |grep ^$1)
+    for project in $(ls)
     do
-        echo -e "\033[42;37m $project \033[0m"
-        cd  "${gitlabpath}/$project"
-        pwd
-        git pull
+        echo -e "\033[42;37m Project : \033[42;37;1m$project \033[0m"
+        if [ ! -d "${gitlabpath}/$project" ]; then
+        	echo "${gitlabpath}/$project not a folder"
+        else
+            cd  "${gitlabpath}/$project"
+
+            if [ ! -d "${gitlabpath}/$project/.git" ]; then
+                echo "${gitlabpath}/$project not a git repo"
+            else
+    	        pwd
+                git pull
+            fi
+        fi
     done
 }
 
-if [ -n "$1" ];then
-    #    pullPlatform path-
-    pullPlatform "$1-"
-# else
-    
-    # for path in ${allow_path_a[@]} do
-    # #    pullPlatform path-
-    #     echo "${path}"
-    #     pullPlatform $path"-"
-
-    # done
-
-fi
+pullPlatform 
+# pullPlatform rn-
+# pullPlatform ncwz-
+# pullPlatform qingniu-
+# pullPlatform we-
+# pullPlatform qingteng-
+# pullPlatform crowdsource-
 
 
+echo -e "\033[44;37m ==========            \033[44;37;1mEnd            \033[0m\033[44;37m========== \033[0m"
 
-
-
-
-echo "======================== +++++ ========================"
-echo -e "==========                \033[42;37mend\033[0m                =========="
-echo "======================== +++++ ========================"
+echo ""
