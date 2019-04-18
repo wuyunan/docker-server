0d91edb9c24fb8c131e464126d7173507223c607
wuyunan <wuyunan@qingteng.info>
2019-04-18 15:19:47 +0800

    v1

--- dubbo/dubbo_admin.sh
+++ dubbo/dubbo_admin.sh
@@ -2,7 +2,7 @@
 
 docker run -d \
 -p 8080:8080 \
--e dubbo.registry.address=zookeeper://192.168.1.170:2181 \
+-e dubbo.registry.address=zookeeper://115.159.149.28:2181 \
 -e dubbo.admin.root.password=root \
 -e dubbo.admin.guest.password=guest \
 chenchuxin/dubbo-admin 
