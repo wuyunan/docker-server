a70c041d7355b9727103f202d7f4d6dd26ed4b20
wuyunan <511083+wuyunan@users.noreply.github.com>
2019-12-04 14:25:04 +0800

    v1

--- vultr/init_server.sh
+++ vultr/init_server.sh
@@ -3,7 +3,7 @@
 IP=`vultr-cli server list|awk '{if($1!="ID"){print $2}}'`
 echo $IP
 user_host=root@${IP}
-#ssh-keyscan -H ${host}
+ssh-keyscan -H ${host}
 
 ssh-keyscan -t rsa -H $IP >> ~/.ssh/known_hosts
 scp init.sh ${user_host}:.
