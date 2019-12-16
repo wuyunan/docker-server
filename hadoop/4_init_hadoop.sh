78c1faf7e360feb03613201fdfc792d1084e8158
wuyunan <511083+wuyunan@users.noreply.github.com>
2019-12-16 11:50:35 +0800

    v1

--- hadoop/4_init_hadoop.sh
+++ hadoop/4_init_hadoop.sh
@@ -4,6 +4,6 @@
 . ./_head.sh
 
 
-scp -r  ./hadoop/etc/hadoop $user@node$N:$work_dir/_app/$hadoop
+scp -r  ./hadoop/etc/hadoop $user@node1:$work_dir/_app/$hadoop/etc/
 
-# for N in $(seq 2 4); do scp -r $work_dir/_app/$hadoop $user@node$N:$work_dir/_app/; done;
+for N in $(seq 2 4); do scp -r $work_dir/_app/$hadoop $user@node$N:$work_dir/_app/; done;
