9afede0c39784eda624b1c1383eb048df379f7c5
wuyunan <511083+wuyunan@users.noreply.github.com>
2019-12-20 11:54:43 +0800

    v

--- hadoop/5_hadoop_hdfs_start.sh
+++ hadoop/5_hadoop_hdfs_start.sh
@@ -2,5 +2,16 @@
 # author:wuyunan
 # url:www.qingteng.info
 . ./_head.sh
+for N in $(seq 1 1); do ssh $user@node$N hdfs --daemon start journalnode; done;
 
-for N in $(seq 1 4); do ssh $user@node$N hdfs --daemon start journalnode;jps; done;
+ssh $user@node1 hdfs --daemon start namenode && jps
+
+ssh $user@node2 hdfs namenode -bootstrapStandby
+
+ssh $user@node2 hdfs --daemon start namenode && jps
+
+
+ssh $user@node1 hdfs haadmin -getAllServiceState
+ssh $user@node2 hdfs haadmin -getAllServiceState
+
+ssh $user@node2 start-dfs.sh
