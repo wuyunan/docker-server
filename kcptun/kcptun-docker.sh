a16393ca7bd91249bcdf5f7affd6cee6fa52d3c2
wuyunan <511083+wuyunan@users.noreply.github.com>
2019-12-27 12:28:17 +0800

    v

--- kcptun/kcptun-docker.sh
+++ kcptun/kcptun-docker.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-docker run -d -p 8388:8388 -p 29900:29900/udp zetaplusae/shadowsocks-kcptun \
+docker run -d -p 443:8388 -p 29900:29900/udp zetaplusae/shadowsocks-kcptun \
     -m 'aes-256-cfb' \
     -p 'wuyunan+' \
     -s "wuyunan" \
