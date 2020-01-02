30d12fac5f6b87d4953fa952db16bed0a86e5a79
wuyunan <511083+wuyunan@users.noreply.github.com>
2020-01-02 22:39:37 +0800

    v

--- mysql/mysql.sh
+++ mysql/mysql.sh
@@ -2,8 +2,8 @@
 docker run \
 --name first-mysql \
 -p 3306:3306 \
--e MYSQL\_ROOT\_PASSWORD=root \
--e MYSQL\_DATABASE=test \
+-e MYSQL\_ROOT\_PASSWORD=Password01! \
+-e MYSQL\_DATABASE=qt \
 -d mysql \
 --default-authentication-plugin=mysql_native_password \
 --max-allowed-packet=32505856
